c8349d5e27759de43d975bc9141ebc5cf6ecb551
RahulPrashar27 <rprasharr@gmail.com>
2026-05-12 12:37:19 +0530

    uc9

---
+++ src/main/java/com/quantity/measurement/MeasurementApplication.java
@@ -0,0 +1,17 @@
+package com.quantity.measurement;
+
+import com.quantity.measurement.enums.LengthUnit;
+import com.quantity.measurement.model.QuantityLength;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementApplication {
+
+	public static void main(String[] args) {
+
+		QuantityLength q1 = new QuantityLength(2.0, LengthUnit.CENTIMETERS);
+		QuantityLength q2 = new QuantityLength(2.0, LengthUnit.YARDS);
+
+		System.out.println(q1.equals(q2));
+	}
+}
